8d3afb1fb0f0b7ee9bd992f56e13cbf4ef068aca
takako916 <takako916@users.noreply.github.com>
2025-10-28 16:33:19 +0100

    sitemap updated  Changes to be committed: 	modified:   app/sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -23,6 +23,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly' as const,
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/claquettes`,
+      lastModified: currentDate,
+      changeFrequency: 'weekly' as const,
+      priority: 0.8,
+    },
   ]
 }
 
